d7e28d6639e84a3561668dcafb5d247155985913
Rohan Vachhani <rohan.vachhani5@gmail.com>
2020-04-04 19:24:49 -0400

    Custom information dialog added

---
+++ app/src/main/java/com/rohan/hacksandfacts/CustomDialogFragment.java
@@ -0,0 +1,51 @@
+package com.rohan.hacksandfacts;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class CustomDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.custom_fragment_dialog, container, false);
+
+
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            getDialog().getWindow().setGravity(Gravity.CENTER);
+            getDialog().setCancelable(false);
+            getDialog().setCanceledOnTouchOutside(false);
+        }
+        TextView ok_text_view = view.findViewById(R.id.ok_button);
+
+        ok_text_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Fragment f = getFragmentManager().findFragmentByTag("customDialogFragment");
+                if (f != null) {
+                    ft.remove(f);
+                }
+                ft.commit();
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/rohan/hacksandfacts/SubmitSuggestion.java
+++ app/src/main/java/com/rohan/hacksandfacts/SubmitSuggestion.java
@@ -1,21 +1,29 @@
 package com.rohan.hacksandfacts;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SubmitSuggestion extends AppCompatActivity implements View.OnClickListener {
+public class SubmitSuggestion extends FragmentActivity implements View.OnClickListener {
 
-    ImageView instagramIcon, gmailIcon, mainTextImageViews;
+    ImageView instagramIcon, gmailIcon, mainTextImageViews, info_button;
     ImageButton mainBackButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +34,22 @@ public class SubmitSuggestion extends AppCompatActivity implements View.OnClickL
         gmailIcon = findViewById(R.id.gmail_icon);
         mainBackButton = findViewById(R.id.main_black_button);
         mainTextImageViews = findViewById(R.id.main_image_view);
+        info_button = findViewById(R.id.info_button);
 
         instagramIcon.setOnClickListener(this);
         gmailIcon.setOnClickListener(this);
         mainBackButton.setOnClickListener(this);
-
-//        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
-//        // Use bounce interpolator with amplitude 0.2 and frequency 20
-//        AnimationBounceInterpolator interpolator = new AnimationBounceInterpolator(0.7, 30);
-//        myAnim.setInterpolator(interpolator);
-//        mainBackButton.startAnimation(myAnim);
+        info_button.setOnClickListener(this);
 
         Animation b = AnimationUtils.loadAnimation(this, R.anim.slide_up);
         b.setStartOffset(500);
-
         mainBackButton.startAnimation(b);
-
     }
-
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finish();
-
     }
 
     @Override
@@ -58,9 +58,7 @@ public class SubmitSuggestion extends AppCompatActivity implements View.OnClickL
             case R.id.instagram_icon:
                 Uri uri = Uri.parse("http://instagram.com/_u/hacks_and_facts/");
                 Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
-
                 likeIng.setPackage("com.instagram.android");
-
                 try {
                     startActivity(likeIng);
                 } catch (ActivityNotFoundException e) {
@@ -80,6 +78,10 @@ public class SubmitSuggestion extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.main_black_button:
                 onBackPressed();
+                break;
+            case R.id.info_button:
+                CustomDialogFragment customDialogFragment = new CustomDialogFragment();
+                customDialogFragment.show(getSupportFragmentManager(), "customDialogFragment");
                 break;
         }
     }
